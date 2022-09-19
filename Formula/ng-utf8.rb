67fb4cb5373c7725f615e64f267012a26268a098
MACHIDA Hideki <h@matchy.net>
2022-09-19 12:18:54 +0900

    なんかうまくいかないので/etc/skelにコピーしてみる

--- Formula/ng-utf8.rb
+++ Formula/ng-utf8.rb
@@ -34,9 +34,10 @@ class NgUtf8 < Formula
     mkdir_p "#{prefix}/share"
     system "make install"
     homerc = %Q(#{Etc.getpwuid.dir}/.ng)
+    dstrc = %Q(/etc/skel/.ng)
     tmprc  = %Q(#{ENV['HOMEBREW_LIBRARY']}/Taps/matchy256/homebrew-matchy/Resources/ng-utf8/dot.ng)
     unless (File.exists?(homerc)) then
-      system "cp #{tmprc} #{homerc}"
+      cp tmprc, dstrc
     end
   end
 end
